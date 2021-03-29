60d4e23d203cc697e28ea050e4a8db5d4b2bd27f
Miro Hrončok <miro@hroncok.cz>
2021-03-29 13:51:24 +0200

    Release 0.0.6

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author_email="miro@hroncok.cz",
     url="https://github.com/fedora-python/tox-current-env",
     license="MIT",
-    version="0.0.5",
+    version="0.0.6",
     package_dir={"": "src"},
     packages=find_packages("src"),
     entry_points={"tox": ["current-env = tox_current_env.hooks"]},

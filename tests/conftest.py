028ee0cfb4ee6ada3a094bdd6d5f6c538f6a6fad
Miro Hrončok <miro@hroncok.cz>
2022-12-13 16:56:33 +0100

    Avoid race conditions when running tests with xdist

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 import shutil
 
 import pytest
@@ -5,12 +6,15 @@ from utils import FIXTURES_DIR, TOX4
 
 
 @pytest.fixture(autouse=True)
-def projdir(tmp_path, monkeypatch):
+def projdir(tmp_path, monkeypatch, worker_id):
     pwd = tmp_path / "projdir"
     pwd.mkdir()
     for fname in "tox.ini", "setup.py":
         shutil.copy(FIXTURES_DIR / fname, pwd)
     monkeypatch.chdir(pwd)
+    # https://github.com/pypa/pip/issues/5345#issuecomment-386424455
+    monkeypatch.setenv("XDG_CACHE_HOME",
+                       os.path.expanduser(f"~/.cache/pytest-xdist-{worker_id}"))
     return pwd
 
 

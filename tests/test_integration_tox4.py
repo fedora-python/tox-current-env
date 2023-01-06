f95e06e6b2ae212193ceed5cfd2e1f4643c6d49a
Lumir Balhar <lbalhar@redhat.com>
2023-01-06 12:41:15 +0100

    Support report of installed packages in CI
    Fixes: https://github.com/fedora-python/tox-current-env/issues/63

--- tests/test_integration_tox4.py
+++ tests/test_integration_tox4.py
@@ -421,3 +421,11 @@ def test_pass_env(projdir, pass_env):
     assert result.returncode == 0
     assert "\nassertme\n" in result.stdout
     assert "\nNone\n" not in result.stdout
+
+
+def test_report_installed(projdir):
+    env = {"TRAVIS": "true"}
+    result = tox("-e", NATIVE_TOXENV, "--current-env", env=env, quiet=False)
+    assert result.returncode == 0
+    assert "tox==" in result.stdout
+    assert "pytest==" in result.stdout
